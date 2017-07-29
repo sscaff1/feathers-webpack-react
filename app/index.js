436f16051627427d5f2db1480ff74486754a12d8
Steven Scaffidi <sscaff1@gmail.com>
2017-07-29 10:09:30 -0700

    refactoring this repo

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>React Webpack Feathers setup done!</h1>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: [
+    'webpack-hot-middleware/client?http://localhost:3030/', // new
+    'webpack/hot/only-dev-server', // new
+    './app/index.js',
+  ],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    inline: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_module/,
+        use: [
+          { loader: 'react-hot-loader' },
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015', 'react', 'stage-2'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+};
